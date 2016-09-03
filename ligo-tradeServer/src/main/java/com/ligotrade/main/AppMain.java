de7d04e579e7e04b0001fa92c67afc32d52d862f
zhiya.chai <zhiya.chai@172.16.32.245>
2016-09-03 15:49:16 +0800

    update file name

--- ligo-tradeServer/src/main/java/com/ligotrade/main/AppMain.java
+++ ligo-tradeServer/src/main/java/com/ligotrade/main/AppMain.java
@@ -3,27 +3,15 @@
  */
 package com.ligotrade.main;
 
-import com.ligotrade.common.TradeContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * @author ming.li
- *
+ * ∆Ù∂Ø¿‡
  */
 public class AppMain {
 	
-	private static final Logger logger = LoggerFactory.getLogger(AppMain.class);
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		TradeContext.getInstance();
-		try {
-			Thread.sleep(10000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 }
