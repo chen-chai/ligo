fd719949c781d3555c4eb56cb8baa2c175e77bc6
chubchen <chenlovejava@163.com>
2016-09-03 16:04:27 +0800

    reset 上一版本

--- ligo-tradeServer/src/main/java/com/ligotrade/main/AppMain.java
+++ ligo-tradeServer/src/main/java/com/ligotrade/main/AppMain.java
@@ -3,25 +3,15 @@
  */
 package com.ligotrade.main;
 
-import com.ligotrade.common.TradeContext;
 
 /**
- * @author ming.li
- *
+ * peng.chen
  */
 public class AppMain {
 	
-	Logger
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		TradeContext.getInstance();
-		try {
-			Thread.sleep(10000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 }
