6f4e479e3e5a4f396f54158e0f1b928f1902c9eb
zhiya.chai <zhiya.chai@172.16.32.245>
2016-11-19 17:31:00 +0800

    增加mix混合服务的代码

---
+++ ligo-common/src/main/java/com/ligo/common/service/mix/VerifyCodeService.java
@@ -0,0 +1,10 @@
+package com.ligo.common.service.mix;
+
+/**
+ * 验证码接口
+ * @author zhiya.chai
+ * 2016年11月19日 下午5:12:39
+ */
+public interface VerifyCodeService {
+
+}
---
+++ ligo-mixserver/src/main/java/com/ligo/mix/service/impl/VerifyCodeServiceImpl.java
@@ -0,0 +1,15 @@
+package com.ligo.mix.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.ligo.common.service.mix.VerifyCodeService;
+
+/**
+ * 验证码服务
+ * @author zhiya.chai
+ * 2016年11月19日 下午5:14:13
+ */
+@Service
+public class VerifyCodeServiceImpl implements VerifyCodeService {
+
+}
